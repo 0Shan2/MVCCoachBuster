123a4fc41644eecf68c74004c15b581d5b580033
Cristina <Cris@wodbuster.com>
2023-10-06 15:34:00 +0200

    Agregar archivos de proyecto.

---
+++ MVCCoachBuster/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCoachBuster.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
