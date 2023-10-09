59f30c58e80cf46f1df27e640a1927c13ecf7593
Chels <Chels@WodBuster.com>
2023-10-09 13:01:59 +0200

    CRUD Planes

---
+++ MVCCoachBuster/Models/Plan.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCCoachBuster.Models
+{
+    public class Plan
+    {
+        public int Id { get; set; }
+
+        //[Required(ErrorMessage = "El nombre del entrenamiento es requerido.")]
+        //[MinLength(2, ErrorMessage = "El nombre del entrenamiento debe ser mayor o igual a 2 caracteres."),
+        //MaxLength(50, ErrorMessage = "El nombre del entrenamiento no debe exceder los 50 caracteres.")]
+        public string Nombre { get; set; }
+
+       // [Display(Name = "Descripción")]
+        //[StringLength(200, MinimumLength = 5,
+                 //ErrorMessage = "La descripción del producto debe contener entre 5 y 200 caracteres.")]
+        public string Descripcion { get; set; }
+        //[Required(ErrorMessage = "El costo es obligatorio.")]
+       // [Column(TypeName = "decimal(18,2)")]
+        public decimal Precio { get; set; }
+
+        public bool Suscrito { get; set; }
+
+        public int UsuarioId { get; set; }
+        public virtual Usuario entrenador { get; set; }
+
+
+    }
+}
