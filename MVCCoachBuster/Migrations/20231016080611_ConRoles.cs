b144d3191f703657b83d655eefd7eb829dd06e7c
Cristina <Cris@wodbuster.com>
2023-10-16 10:46:56 +0200

    SQLServer vuelta a conectar

---
+++ MVCCoachBuster/Migrations/20231016080611_ConRoles.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCCoachBuster.Migrations
+{
+    public partial class ConRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
